826d90fdc69a170d97530dad4605cc82a1990c9b
Wahab Seidu <wahabseidu009@gmail.com>
2023-08-12 23:27:28 +0000

    Allow the decision of each game to be displayed
    A score counter has not yet been added. The decision of
    each round is just displayed as "you win/you lose". The number of
    rounds has not also been specified yet.

--- game.js
+++ game.js
@@ -1,48 +1,72 @@
 function getComputerChoice() {
-    const gameOption = ["rock", "paper", "scissors"];
+    const gameOption = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * gameOption.length);
     let randomSelection = gameOption[randomIndex];
     return randomSelection;
 }
 
-function playRound(playerChoice, computerSelection) {
-    let playerSelection = playerChoice;
+
+const gameKey = document.querySelectorAll(".gameKey");
+
+let clicks = gameKey.forEach((key) => {
+    key.addEventListener("click", () => {
+        playerSelection = key.textContent;
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection)    
+    })
+})
+
+const div = document.querySelector("div");
+
+const roundResult = document.createElement("div");
+div.appendChild(roundResult);
+const para = document.createElement("p");
+const paraComputerChoice = document.createElement("p");
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    paraComputerChoice.textContent = `The Alien choose ${computerSelection}`;
     if (playerSelection === computerSelection) {
-        return "This round is a tie";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats Paper";
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper";
-    } 
-
-}
-
-function game() {
-    let computerSelection = getComputerChoice();
-    let yourScore = 0;
-    let computerScore = 0;
-    for (i = 0; i < 5; i++) {
-        let playerChoice = prompt("Rock!!! Paper!!! Scissors!!!").toLowerCase();
-        console.log(`The player picked ${playerChoice}`);
-        console.log(`The computer picked ${computerSelection}`)
-        console.log(playRound(playerChoice, computerSelection));
-            if ((playerChoice === "rock" && computerSelection === "scissors") || (playerChoice === "paper" && computerSelection === "rock") || 
-            (playerChoice === "scissors" && computerSelection === "paper")) {
-                console.log(`YOU: ${++yourScore}, COMPUTER: ${computerScore}`)
-            } else if ((playerChoice === "rock" && computerSelection === "paper") || (playerChoice === "paper" && computerSelection === "scissors") || 
-            (playerChoice === "scissors" && computerSelection === "rock")) {
-                console.log(`YOU: ${yourScore}, COMPUTER: ${++computerScore}`)
-            } else if (playerChoice === computerSelection) {
-                console.log(`YOU: ${yourScore}, COMPUTER: ${computerScore}`)
-            }
+        para.textContent = "This round is a tie";
+        console.log("This is a tie")
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        para.textContent = "You Lose this round!";
+        console.log("You Lose!")
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        para.textContent = "You Win this round!";
+        console.log("You Win!")
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        para.textContent = "You Win this round!";
+        console.log("You Win!");
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        para.textContent = "You Lose this round!";
+        console.log("You Lose!");
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        para.textContent = "You Lose this round!";
+        console.log("You Lose!")
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        para.textContent = "You Win this round!";
+        console.log("You Win!");
     }
 }
-game();
+
+roundResult.appendChild(paraComputerChoice);
+roundResult.appendChild(para);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
