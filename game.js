cbb3a479023ab612231ea748336b8a4333e4ed10
Wahab Seidu <wahabseidu009@gmail.com>
2023-08-27 01:40:14 +0000

    Complete the styling of the game.
    At this point, the game has a nice visual appeal, and a text link
    shown to a player after the game to play another game.

--- game.js
+++ game.js
@@ -27,32 +27,47 @@ gameKey.forEach((key) => {
 });
 
 const body = document.querySelector("body");
-const script = document.querySelector("script")
+const buttonDiv = document.querySelector("div")
+const script = document.querySelector("script");
+
+const scoreDiv = document.createElement("div");
+const userScorePara = document.createElement("p");
+const computerScorePara = document.createElement("p");
+
+scoreDiv.classList.add("score-counter");
+body.insertBefore(scoreDiv, buttonDiv);
+scoreDiv.appendChild(userScorePara);
+scoreDiv.appendChild(computerScorePara);
 
 const roundResult = document.createElement("div");
 roundResult.classList.add("results");
-body.insertBefore(roundResult, script)
+body.insertBefore(roundResult, script);
 
 const para = document.createElement("p");
-const paraComputerChoice = document.createElement("p");
-const roundPara = document.createElement("p")
-const scorePara = document.createElement("p");
+const roundPara = document.createElement("p");
+
+const playAgain = document.createElement("a");
+playAgain.setAttribute("href", "file:///home/swordmachina/repo/RPS-game/index.html")
 
 
-function playRound(playerSelection, computerSelection) {
-    paraComputerChoice.textContent = `The Alien chose ${computerSelection}`;
-    
+function playRound(playerSelection, computerSelection) { 
     if (playerSelection === computerSelection) {
         para.textContent = "This round is a tie";
+        computerScorePara.style.color = "blue";
+        userScorePara.style.color = "blue";
     } else if (
         (playerSelection === "Rock" && computerSelection === "Scissors") ||
         (playerSelection === "Paper" && computerSelection === "Rock") ||
         (playerSelection === "Scissors" && computerSelection === "Paper")
     ) {
-        para.textContent = "You Win this round!";
+        para.textContent = `The Alien chose ${computerSelection}, You Win this round!`;
+        userScorePara.style.color = "green";
+        computerScorePara.style.color = "crimson";
         youScore++;
     } else {
-        para.textContent = "You Lose this round!";
+        para.textContent = `The Alien chose ${computerSelection}, You Lose this round!`;
+        computerScorePara.style.color = "green";
+        userScorePara.style.color = "crimson";
         computerScore++;
     }
 }
@@ -61,21 +76,30 @@ function scoreCounter() {
     if (clickCount === maxClicks) {
         if (youScore > computerScore) {
             para.textContent = "You win the game!";
-            
+            computerScorePara.style.color = "crimson";
+            userScorePara.style.color = "green";
+            playAgain.textContent = "play Again"
         } else if (youScore < computerScore) {
             para.textContent = "Computer wins the game!";
+            computerScorePara.style.color = "green";
+            userScorePara.style.color = "crimson";
+            playAgain.textContent = "playAgain"
         } else {
             para.textContent = "It's a tie game!";
+            playAgain.textContent = "playAgain"
         }
     }
-    scorePara.textContent = `YOU: ${youScore}  COMPUTER: ${computerScore}`
-    roundPara.textContent = `ROUND: ${clickCount}`
+    userScorePara.textContent = `YOU: ${youScore}` 
+    computerScorePara.textContent =  `COMPUTER: ${computerScore}`
+    roundPara.textContent = `ROUND ${clickCount}`
 }
 
-roundResult.appendChild(paraComputerChoice);
-roundResult.appendChild(para);
+
+
 roundResult.appendChild(roundPara);
-roundResult.appendChild(scorePara);
+roundResult.appendChild(para);
+roundResult.appendChild(playAgain);
+
 
 
 
